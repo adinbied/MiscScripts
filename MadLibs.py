5013374b56c5685c564237084dbd0518400da32a
adinbied <adinbied@gmail.com>
2017-02-24 10:25:07 -0800

    Update MadLibs.py

--- MadLibs.py
+++ MadLibs.py
@@ -1,7 +1,11 @@
+#Python Mad Libs
 print("Welcome to Mad Libs, Python Edition!")
 time.sleep(1)
-story = raw_input("Please select a story. A:Story1, B:Story2, C:Story3")
-print("Story " + story + "selected! 
-noun1 = raw_input("Noun? ")
-verb1 = raw_input("Verb? ")
-print("So, you are already " + verb1 + " years old, " + noun1 + "!")
+story = raw_input("Please select a story. A: Story1, B: Story2, C: Story3") #Select a story
+print("Story " + story + "selected! Please wait... ")
+time.sleep(1)
+if story == "A"
+  then noun1 = raw_input("Noun? ")
+  verb1 = raw_input("Verb? ")
+  print("So, you are already " + verb1 + " years old, " + noun1 + "!")
+  
