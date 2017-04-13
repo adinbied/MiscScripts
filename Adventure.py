889fdfff6ebc9aba890921a39c4020e1b8788e7a
adinbied <adinbied@gmail.com>
2017-04-13 13:55:41 -0700

    Update Adventure.py

--- Adventure.py
+++ Adventure.py
@@ -1,2 +1,16 @@
+#Python Text-Based adventure game
+#By Adin Biederman
 import time #get sleep dependencies
-Print
+print("Welcome to the mystical Python adventure!")
+playername = raw_input("Please enter your name: ")
+print("Hello" + playername + ". This game will take you through a series of puzzles and challenges.")
+beginq = raw_input("Are you ready to begin? Say yes or no: ")
+  if beginq == 'yes' or beginq == 'Yes':
+    print("OK, then let's go!")
+    print("You enter a large cavern, that is completely black.")
+    cavern1 = raw_input("Which way do you go? Say North or South: ")
+      if cavern1 == 'North' or 'north':
+    
+  else:
+    print("Sorry to hear that. Goodbye!")
+    return
